249c8723277f42e8d4c836198c9fd8cf14860859
Ksenia <kseniatravnikova@duck.com>
2025-11-03 01:18:44 +0300

    [+] Форма для пароля

---
+++ passwords/admin/forms.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+
+from passwords.models.password import Password
+
+
+class PasswordAdminForm(forms.ModelForm):
+	password_plain = forms.CharField(
+		required=False,
+		label="Plain password",
+		widget=forms.PasswordInput(render_value=False),
+		help_text="Введите новый пароль (оставьте пустым, чтобы не менять)",
+	)
+
+	class Meta:
+		model = Password
+		exclude = ("password",)
+
+	def save(self, commit=True):
+		instance = super().save(commit=False)
+		if self.cleaned_data.get("password_plain"):
+			instance.set_password(self.cleaned_data["password_plain"])
+		if commit:
+			instance.save()
+			self.save_m2m()
+		return instance
