5b702eac3708c5a1e094fef33a165edef2dc0ca2
Ksenia <kseniatravnikova@duck.com>
2025-11-03 01:10:04 +0300

    [~] Шаблон для приложений

---
+++ app_template/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# from django.apps import AppConfig
+
+# class AppNameConfig(AppConfig):
+# default_auto_field = 'django.db.models.BigAutoField'
+# name = 'app_name'
+# verbose_name = 'App Name'
