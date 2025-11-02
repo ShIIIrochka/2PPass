2684eff725d2a55e3c40c8dc88afc809b68009b2
Ksenia <kseniatravnikova@duck.com>
2025-11-03 01:20:10 +0300

    [*] Роуты приложения

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
-"""
-URL configuration for config project.
+"""URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
+
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from passwords.admin.admin import user_admin_site
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path("admin/", admin.site.urls),
+	path("console/", user_admin_site.urls),
 ]
