2f4cacb3f1f7122d639719e2ca73392d78ce4131
Ksenia <kseniatravnikova@duck.com>
2025-11-05 01:02:16 +0300

    [+] Модель тегов

---
+++ passwords/models/tag.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=127, unique=True)
+
+	def __str__(self):
+		return self.name
