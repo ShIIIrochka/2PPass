28d265459f96da886c5aeb31831d09df8c8047f7
Ksenia <kseniatravnikova@duck.com>
2025-11-05 04:25:57 +0300

    [+] утилита для получения или создании джанги группы

--- passwords/signals/utils.py
+++ passwords/signals/utils.py
@@ -1,9 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from passwords.models.password import Password
 
 
 def get_or_create_group(name: str) -> Group:
-	"""Получить или создать Django-группу."""
-	group, _ = Group.objects.get_or_create(name=name)
+	"""Получить или создать Django-группу с нужными permissions."""
+	group, created = Group.objects.get_or_create(name=name)
+
+	if created:
+		if name == "Password Viewers":
+			perms = ["view_password"]
+		elif name == "Password Editors":
+			perms = [
+				"view_password",
+				"add_password",
+				"change_password",
+				"delete_password",
+			]
+		else:
+			perms = []
+
+		if perms:
+			content_type = ContentType.objects.get_for_model(Password)
+			permissions = Permission.objects.filter(
+				content_type=content_type, codename__in=perms
+			)
+			group.permissions.set(permissions)
+
 	return group
